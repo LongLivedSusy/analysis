dcce220aa8adfad58a2fd73c122c1d4787d3e1b7
Viktor Kutzner <viktor.kutzner@cern.ch>
2019-06-25 15:25:11 +0200

    wip

--- background-estimation/non-prompt/prepare_template_fit.py
+++ background-estimation/non-prompt/prepare_template_fit.py
@@ -8,7 +8,16 @@ import uuid
 from GridEngineTools import runParallel
 import collections
 
-def get_histos():
+def get_histos(config, index):
+
+    histos = collections.OrderedDict()
+    for label in config:
+        histos[label] = get_histogram(config[label])
+
+        
+def get_configurations():
+
+    configurations = collections.OrderedDict()
 
     path = "output_skim_11_merged"
     mc_background = "Summer16.DYJetsToLL|Summer16.QCD|Summer16.WJetsToLNu|Summer16.ZJetsToNuNu_HT|Summer16.WW_TuneCUETP8M1|Summer16.WZ_TuneCUETP8M1|Summer16.ZZ_TuneCUETP8M1|Summer16.TTJets_TuneCUETP8M1_13TeV"
@@ -25,32 +34,41 @@ def get_histos():
              "noleptons":   "passesUniversalSelection==1 && MHT>250 && MinDeltaPhiMhtJets>0.3 && n_jets>0 && n_leptons==0",
            }
 
+    tags = {
+             "loose1":      {"short": " && tracks_mva_bdt_loose>0 ", "long": " && tracks_mva_bdt_loose>0 ", "track_SR": " && tracks_dxyVtx<=0.01 ", "track_CR": " && tracks_dxyVtx>0.01 "},
+           }
+
     for variable in variables:
+
         for label in cuts:
 
-            control_region = cuts[label]
-            track_selection = variables[variable][0] + " && tracks_mva_bdt_loose>0 "
-            track_SR = " && tracks_dxyVtx<=0.01 "
-            track_CR = " && tracks_dxyVtx>0.01 "
-            
-            nBinsX = variables[variable][1]
-            xmin = variables[variable][2]
-            xmax = variables[variable][3]
+            for tag in tags:
 
-            fakelike           = control_region + track_selection + track_CR
-            promptlike         = control_region + track_selection + track_SR + " && tracks_is_reco_lepton==1 "
-            background_tracks  = control_region + track_selection + track_SR
-            signal_tracks      = control_region + track_selection + track_SR
+                control_region = cuts[label]
+                track_selection = variables[variable][0]
+                if "pixel_track==1" in track_selection:
+                    track_selection += tags[tag]["short"]
+                elif "pixel_track==0" in track_selection:
+                    track_selection += tags[tag]["long"]
+              
+                nBinsX = variables[variable][1]
+                xmin = variables[variable][2]
+                xmax = variables[variable][3]
 
-            histos["%s_bg_%s" % (variable, label)] =             get_histogram(variable, background_tracks, nBinsX=nBinsX, xmin=xmin, xmax=xmax, path=path, selected_sample=mc_background)
-            histos["%s_promptlike_%s" % (variable, label)] =     get_histogram(variable, promptlike, nBinsX=nBinsX, xmin=xmin, xmax=xmax, path=path, selected_sample=mc_background)
-            histos["%s_fakelike_%s" % (variable, label)] =       get_histogram(variable, fakelike, nBinsX=nBinsX, xmin=xmin, xmax=xmax, path=path, selected_sample=mc_background)
-            histos["%s_signal_ctau10_%s" % (variable, label)] =  get_histogram(variable, signal_tracks, nBinsX=nBinsX, xmin=xmin, xmax=xmax, path=path, selected_sample="Summer16.g1800_chi1400_27_200970_step4_10AODSIM")
-            histos["%s_signal_ctau30_%s" % (variable, label)] =  get_histogram(variable, signal_tracks, nBinsX=nBinsX, xmin=xmin, xmax=xmax, path=path, selected_sample="Summer16.g1800_chi1400_27_200970_step4_30AODSIM")
-            histos["%s_signal_ctau50_%s" % (variable, label)] =  get_histogram(variable, signal_tracks, nBinsX=nBinsX, xmin=xmin, xmax=xmax, path=path, selected_sample="Summer16.g1800_chi1400_27_200970_step4_50AODSIM")
-            histos["%s_signal_ctau100_%s" % (variable, label)] = get_histogram(variable, signal_tracks, nBinsX=nBinsX, xmin=xmin, xmax=xmax, path=path, selected_sample="Summer16.g1800_chi1400_27_200970_step4_100AODSIM")
+                fakelike           = control_region + track_selection + tags[tag]["track_CR"]
+                promptlike         = control_region + track_selection + tags[tag]["track_SR"] + " && tracks_is_reco_lepton==1 "
+                background_tracks  = control_region + track_selection + tags[tag]["track_SR"]
+                signal_tracks      = control_region + track_selection + tags[tag]["track_SR"]
 
-    return histos
+                configurations["%s_%s_bg_%s" % (variable, tag, label)] =             [variable, background_tracks, nBinsX=nBinsX, xmin=xmin, xmax=xmax, path=path, selected_sample=mc_background]
+                configurations["%s_%s_promptlike_%s" % (variable, tag, label)] =     [variable, promptlike, nBinsX=nBinsX, xmin=xmin, xmax=xmax, path=path, selected_sample=mc_background]
+                configurations["%s_%s_fakelike_%s" % (variable, tag, label)] =       [variable, fakelike, nBinsX=nBinsX, xmin=xmin, xmax=xmax, path=path, selected_sample=mc_background]
+                configurations["%s_%s_signal_ctau10_%s" % (variable, tag, label)] =  [variable, signal_tracks, nBinsX=nBinsX, xmin=xmin, xmax=xmax, path=path, selected_sample="Summer16.g1800_chi1400_27_200970_step4_10AODSIM"]
+                configurations["%s_%s_signal_ctau30_%s" % (variable, tag, label)] =  [variable, signal_tracks, nBinsX=nBinsX, xmin=xmin, xmax=xmax, path=path, selected_sample="Summer16.g1800_chi1400_27_200970_step4_30AODSIM"]
+                configurations["%s_%s_signal_ctau50_%s" % (variable, tag, label)] =  [variable, signal_tracks, nBinsX=nBinsX, xmin=xmin, xmax=xmax, path=path, selected_sample="Summer16.g1800_chi1400_27_200970_step4_50AODSIM"]
+                configurations["%s_%s_signal_ctau100_%s" % (variable, tag, label)] = [variable, signal_tracks, nBinsX=nBinsX, xmin=xmin, xmax=xmax, path=path, selected_sample="Summer16.g1800_chi1400_27_200970_step4_100AODSIM"]
+
+    return configurations
 
 
 def plot(histos):
@@ -174,14 +192,37 @@ def plot(histos):
     canvas.SaveAs(canvas_label + ".pdf")
 
 
-if __name__ == "__main__":
-    
-    histos = get_histos()
-    fout = TFile("templates.root", "recreate")
-    for label in histos:
-        histos[label].SetName(label)
-        histos[label].Write()
-    fout.Close()
 
-    
-    
+if __name__ == "__main__":
+
+    parser = OptionParser()
+    parser.add_option("--index", dest="index", default=False)
+    parser.add_option("--runmode", dest="runmode", default="grid")
+    parser.add_option("--hadd", dest="hadd", action="store_true")
+    (options, args) = parser.parse_args()
+
+    configurations = get_configurations()
+
+    if options.index:
+        
+        histogram = get_histogram(*configurations[configurations.keys()[int(options.index)]])
+        label = configurations.keys()[int(options.index)]
+
+        fout = TFile("template_pt%s.root" % options.index, "recreate")
+        histogram.SetName(label)
+        histogram.Write()
+        fout.Close()
+
+    elif options.hadd:
+
+        os.system("hadd -f template.root template_pt*root && rm template_pt*root")
+
+    else:
+
+        commands = []
+        for i in range(len(configurations)):
+            commands.append("./prepare_template_fit.py --index %s" % i)
+        print "running %s jobs!" % len(commands)
+        runParallel(commands, options.runmode, condorDir = "prepare_template_fit_condor", dontCheckOnJobs=False)
+
+
