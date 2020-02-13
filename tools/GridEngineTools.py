41eb2b422fc6fca555ddfaa5c9e23a10d1d29faf
Viktor Kutzner <viktor.kutzner@cern.ch>
2020-02-13 17:39:16 +0100

    updated grid submission tools

--- tools/GridEngineTools.py
+++ tools/GridEngineTools.py
@@ -1,19 +1,36 @@
 #!/bin/env python
-# comments: viktor.kutzner@desy.de
-
 import os, sys
 from glob import glob
 from time import sleep
 import datetime
 import subprocess
 import multiprocessing
+from optparse import OptionParser
+import time
 import commands
+
+# condor grid submission tool
+# comments: viktor.kutzner@desy.de
+#
+# ===============mysubmit.py================
+# import os
+# import GridEngineTools
+# 
+# os.system("mkdir -p test")
+# commands = ["ls -l > test/out1", "ls -lha > test/out2", "whoami > test/out3"]
+# GridEngineTools.runParallel(commands, "grid")
+# # ===============mysubmit.py================
+#
+# $ ./mysubmit.py                         # submit jobs
+# $ python GridEngineTools.py             # check jobs
+# $ python GridEngineTools.py --resubmit  # resubmit failed jobs
+
 
 def ShellExec(command):
     os.system(command)
 
 
-def runParallel(mycommands, runmode, condorDir="bird", cmsbase=False, qsubOptions=False, ncores_percentage=0.60, dontCheckOnJobs=True, use_more_mem=False, use_more_time=False, confirm=True, babysit = True):
+def runParallel(mycommands, runmode, condorDir="condor", cmsbase=False, qsubOptions=False, ncores_percentage=0.60, dontCheckOnJobs=True, use_more_mem=False, use_more_time=False, confirm=True, babysit = True):
 
     if runmode == "multiprocessing" or runmode == "multi" or runmode == "single":
 
@@ -66,7 +83,7 @@ def babysit_jobs(condorDir):
     return 0
 
 
-def runCommands(mycommands, condorDir="bird", cmsbase=False, qsubOptions=False, dontCheckOnJobs=False, useGUI=False, use_more_mem=False, use_more_time=False, confirm=True, babysit=True):
+def runCommands(mycommands, condorDir="condor", cmsbase=False, qsubOptions=False, dontCheckOnJobs=False, useGUI=False, use_more_mem=False, use_more_time=False, confirm=True, babysit=True):
 
     jobscript = '''#!/bin/bash
     echo "$QUEUE $JOB $HOST"
@@ -162,4 +179,91 @@ def runCommands(mycommands, condorDir="bird", cmsbase=False, qsubOptions=False, 
     if babysit:
         status = babysit_jobs(condorDir)
         return status
+
+
+def get_info(condor_dir, showfailed = False):
+
+    #status, failed_jobs = commands.getstatusoutput("grep Failed %s/*.sh.o* | wc -l" % condor_dir)
+    status, failed_jobs = commands.getstatusoutput("grep -L 'Normal termination' %s/*.log | wc -l" % condor_dir)
+    #status, succeeded_jobs = commands.getstatusoutput("grep Success %s/*.sh.o* | wc -l" % condor_dir)
+    status, succeeded_jobs = commands.getstatusoutput("grep 'Normal termination' %s/*.log | wc -l" % condor_dir)
+    status, all_jobs = commands.getstatusoutput("wc -l %s/args" % condor_dir)
+
+    n_failed_jobs = int(failed_jobs.split()[0])
+    n_succeeded_jobs = int(succeeded_jobs.split()[0])
+    n_all = int(all_jobs.split()[0])
+
+    print "%s / %s jobs succeeded (%s failed): %.2f done" % (n_succeeded_jobs, n_all, n_failed_jobs, float(n_succeeded_jobs)/n_all)
+
+    if showfailed and failed_jobs>0:
+        os.system("grep -L 'Normal termination' %s/*.log" % condor_dir)
+
+    return {"success": n_succeeded_jobs, "njobs": n_all, "fail": n_failed_jobs, "percent_done": float(n_succeeded_jobs)/n_all}
+
+
+def resubmit(condor_dir):
+
+    if condor_dir[-1] == "/":
+        condor_dir = condor_dir[:-1]
+
+    status, succeeded_jobs = commands.getstatusoutput("grep Success %s/*.sh.o*" % condor_dir)
+    status, failed_jobs = commands.getstatusoutput("grep -L 'Normal termination' %s/*.log" % condor_dir)
+    #status, failed_jobs = commands.getstatusoutput("grep Failed %s/*.sh.o*" % condor_dir)
+    status, all_args = commands.getstatusoutput("cat %s/args" % condor_dir)
+
+    succeeded_jobs = succeeded_jobs.split("\n")
+    failed_jobs = failed_jobs.split("\n")
+    all_args = all_args.split("\n")
+    failed_args = []
+
+    for i, succeeded_job in enumerate(succeeded_jobs):
+        succeeded_jobs[i] = int( succeeded_job.split("/")[-1].split(".")[0] )
+
+    for i, failed_job in enumerate(failed_jobs):
+        failed_jobs[i] = int( failed_job.split("/")[-1].split(".")[0] )
+       
+    for i_arg, arg in enumerate(all_args):
+        if i_arg not in succeeded_jobs:
+            failed_args.append(arg)
+
+    condor_resubmit_dir = condor_dir + "." + str(int(time.time()))
+    os.system("mkdir -p %s" % condor_resubmit_dir)
+    os.system("cp %s/*.sh %s" % (condor_dir, condor_resubmit_dir))
+    os.system("cp %s/*.submit %s" % (condor_dir, condor_resubmit_dir))
+
+    with open("%s/args" % condor_resubmit_dir, "w+") as fout:
+        fout.write("\n".join(failed_args))
+
+    # update condor queue statement:
+    #os.system("sed -i '/%s/%s' %s/runjobs.submit" % (condor_dir, condor_resubmit_dir, condor_resubmit_dir))
+    os.system("sed -i '/Queue/c\        Queue %s' %s/runjobs.submit" % (len(failed_args)-1, condor_resubmit_dir))
+
+    # update condor output directory path:
+    os.system("sed -i '/initialdir/c\        initialdir = %s' %s/runjobs.submit" % (os.path.abspath(condor_resubmit_dir), condor_resubmit_dir))
+
+    raw_input("Resubmit?...")
+    os.system("cd %s; condor_submit runjobs.submit > cluster_info" % condor_resubmit_dir)
+    babysit_jobs(condor_resubmit_dir)
+
+
+if __name__ == "__main__":
+
+    parser = OptionParser()
+    parser.add_option("--info", dest="info", action="store_true")
+    parser.add_option("--babysit", dest="babysit", action="store_true")
+    parser.add_option("--resubmit", dest="resubmit", action="store_true")
+    (options, args) = parser.parse_args()
+
+    if len(args)>0:
+        condor_dir = args[0]
+    else:
+        jobs.condor = "condor"
+
+    get_info(condor_dir, showfailed = True)
+
+    if options.babysit:
+        babysit_jobs(condorDir)
+    
+    if options.resubmit:
+        resubmit(condor_dir)
 
