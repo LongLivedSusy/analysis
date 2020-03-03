98951362212fd5d136debe8b140175c6b1f02bd3
Viktor Kutzner <viktor.kutzner@cern.ch>
2020-03-03 15:58:39 +0100

    wip

--- analyzer/get_backgrounds.py
+++ analyzer/get_backgrounds.py
@@ -50,8 +50,6 @@ histos["_srSideband_short"] = [" && tracks_SR_short==1 && tracks_deDxHarmonic2pi
 histos["_srSideband_long"] = [" && tracks_SR_long==1 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxSidebandLow, dEdxLow), ""]
 histos["_srMid_short"] = [" && tracks_SR_short==1 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxLow, dEdxMid), ""]
 histos["_srMid_long"] = [" && tracks_SR_long==1 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxLow, dEdxMid), ""]
-histos["_srMidHighDeDx_short"] = [" && tracks_SR_short==1 && tracks_deDxHarmonic2pixel>%s" % (dEdxLow), ""]
-histos["_srMidHighDeDx_long"] = [" && tracks_SR_long==1 && tracks_deDxHarmonic2pixel>%s" % (dEdxLow), ""]
 histos["_srHighDeDx_short"] = [" && tracks_SR_short==1 && tracks_deDxHarmonic2pixel>%s" % (dEdxMid), ""]
 histos["_srHighDeDx_long"] = [" && tracks_SR_long==1 && tracks_deDxHarmonic2pixel>%s" % (dEdxMid), ""]
 
@@ -62,8 +60,6 @@ histos["_fakecrSideband_short"] = [" && tracks_CR_short==1 && tracks_deDxHarmoni
 histos["_fakecrSideband_long"] = [" && tracks_CR_long==1 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxSidebandLow, dEdxLow), ""]
 histos["_fakecrMid_short"] = [" && tracks_CR_short==1 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxLow, dEdxMid), ""]
 histos["_fakecrMid_long"] = [" && tracks_CR_long==1 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxLow, dEdxMid), ""]
-histos["_fakecrMidHighDeDx_short"] = [" && tracks_CR_short==1 && tracks_deDxHarmonic2pixel>%s" % (dEdxLow), ""]
-histos["_fakecrMidHighDeDx_long"] = [" && tracks_CR_long==1 && tracks_deDxHarmonic2pixel>%s" % (dEdxLow), ""]
 histos["_fakecrHighDeDx_short"] = [" && tracks_CR_short==1 && tracks_deDxHarmonic2pixel>%s" % (dEdxMid), ""]
 histos["_fakecrHighDeDx_long"] = [" && tracks_CR_long==1 && tracks_deDxHarmonic2pixel>%s" % (dEdxMid), ""]
 histos["_fakeprediction_short"] = [" && tracks_CR_short==1", "fakerate_short"]
@@ -72,24 +68,38 @@ histos["_fakepredictionSideband_short"] = [" && tracks_CR_short==1 && tracks_deD
 histos["_fakepredictionSideband_long"] = [" && tracks_CR_long==1 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxSidebandLow, dEdxLow), "fakerate_long"]
 histos["_fakepredictionMid_short"] = [" && tracks_CR_short==1 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxLow, dEdxMid), "fakerate_short"]
 histos["_fakepredictionMid_long"] = [" && tracks_CR_long==1 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxLow, dEdxMid), "fakerate_long"]
-histos["_fakepredictionMidHighDeDx_short"] = [" && tracks_CR_short==1 && tracks_deDxHarmonic2pixel>%s" % (dEdxLow), "fakerate_short"]       # **** need to rerun before using that
-histos["_fakepredictionMidHighDeDx_long"] = [" && tracks_CR_long==1 && tracks_deDxHarmonic2pixel>%s" % (dEdxLow), "fakerate_long"]
 histos["_fakepredictionHighDeDx_short"] = [" && tracks_CR_short==1 && tracks_deDxHarmonic2pixel>%s" % (dEdxMid), "fakerate_short"]
 histos["_fakepredictionHighDeDx_long"] = [" && tracks_CR_long==1 && tracks_deDxHarmonic2pixel>%s" % (dEdxMid), "fakerate_long"]
 
 # get prompt background ABCD histograms:
 histos["_lowDeDxPromptElectron"] = [" && (tracks_SR_short+tracks_SR_long)==0 && n_goodelectrons==1 && n_goodmuons==0 && leptons_dedx>%s && leptons_dedx<%s" % (dEdxSidebandLow, dEdxLow), ""]
-histos["_highDeDxPromptElectron"] = [" && (tracks_SR_short+tracks_SR_long)==0 && n_goodelectrons==1 && n_goodmuons==0 && leptons_dedx>%s" % (dEdxLow), ""]
+histos["_midDeDxPromptElectron"] = [" && (tracks_SR_short+tracks_SR_long)==0 && n_goodelectrons==1 && n_goodmuons==0 && leptons_dedx>%s && leptons_dedx<%s" % (dEdxLow, dEdxMid), ""]
+histos["_highDeDxPromptElectron"] = [" && (tracks_SR_short+tracks_SR_long)==0 && n_goodelectrons==1 && n_goodmuons==0 && leptons_dedx>%s" % (dEdxMid), ""]
 histos["_lowDeDxDTnoLep"] = [" && (tracks_SR_short+tracks_SR_long)==1 && n_goodleptons==0 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxSidebandLow, dEdxLow), ""]
-histos["_highDeDxDTnoLep"] = [" && (tracks_SR_short+tracks_SR_long)==1 && n_goodleptons==0 && tracks_deDxHarmonic2pixel>%s" % (dEdxLow), ""]
+histos["_midDeDxDTnoLep"] = [" && (tracks_SR_short+tracks_SR_long)==1 && n_goodleptons==0 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxLow, dEdxMid), ""]
+histos["_highDeDxDTnoLep"] = [" && (tracks_SR_short+tracks_SR_long)==1 && n_goodleptons==0 && tracks_deDxHarmonic2pixel>%s" % (dEdxMid), ""]
 histos["_lowDeDxDT"] = [" && (tracks_SR_short+tracks_SR_long)==1 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxSidebandLow, dEdxLow), ""]
+histos["_midDeDxDT"] = [" && (tracks_SR_short+tracks_SR_long)==1 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxLow, dEdxMid), ""]
 histos["_highDeDxDT"] = [" && (tracks_SR_short+tracks_SR_long)==1 && tracks_deDxHarmonic2pixel>%s" % (dEdxMid), ""]
 
 # get MC Truth histograms for closure
 histos["_srgenfakes_short"] = [" && tracks_SR_short==1 && tracks_fake==1", ""]
 histos["_srgenfakes_long"] = [" && tracks_SR_long==1 && tracks_fake==1", ""]
+histos["_srgenfakesSideband_short"] = [" && tracks_SR_short==1 && tracks_fake==1 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxSidebandLow, dEdxLow), ""]
+histos["_srgenfakesSideband_long"] = [" && tracks_SR_long==1 && tracks_fake==1 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxSidebandLow, dEdxLow), ""]
+histos["_srgenfakesMid_short"] = [" && tracks_SR_short==1 && tracks_fake==1 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxLow, dEdxMid), ""]
+histos["_srgenfakesMid_long"] = [" && tracks_SR_long==1 && tracks_fake==1 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxLow, dEdxMid), ""]
+histos["_srgenfakesHighDeDx_short"] = [" && tracks_SR_short==1 && tracks_fake==1 && tracks_deDxHarmonic2pixel>%s" % (dEdxMid), ""]
+histos["_srgenfakesHighDeDx_long"] = [" && tracks_SR_long==1 && tracks_fake==1 && tracks_deDxHarmonic2pixel>%s" % (dEdxMid), ""]
+
 histos["_srgenprompt_short"] = [" && tracks_SR_short==1 && tracks_fake==0", ""]
 histos["_srgenprompt_long"] = [" && tracks_SR_long==1 && tracks_fake==0", ""]
+histos["_srgenpromptSideband_short"] = [" && tracks_SR_short==1 && tracks_fake==0 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxSidebandLow, dEdxLow), ""]
+histos["_srgenpromptSideband_long"] = [" && tracks_SR_long==1 && tracks_fake==0 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxSidebandLow, dEdxLow), ""]
+histos["_srgenpromptMid_short"] = [" && tracks_SR_short==1 && tracks_fake==0 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxLow, dEdxMid), ""]
+histos["_srgenpromptMid_long"] = [" && tracks_SR_long==1 && tracks_fake==0 && tracks_deDxHarmonic2pixel>%s && tracks_deDxHarmonic2pixel<%s" % (dEdxLow, dEdxMid), ""]
+histos["_srgenpromptHighDeDx_short"] = [" && tracks_SR_short==1 && tracks_fake==0 && tracks_deDxHarmonic2pixel>%s" % (dEdxMid), ""]
+histos["_srgenpromptHighDeDx_long"] = [" && tracks_SR_long==1 && tracks_fake==0 && tracks_deDxHarmonic2pixel>%s" % (dEdxMid), ""]
 
 
 def write_histogram_to_file(variable, cuts, scaling, label, h_suffix, folder, globstrings, output_root_file):
@@ -114,11 +124,12 @@ if __name__ == "__main__":
 
     parser = OptionParser()
     parser.add_option("--index", dest = "index")
+    parser.add_option("--plots_per_job", dest = "plots_per_job", default = 5)
     parser.add_option("--folder", dest = "output_folder", default = "ddbg")
     parser.add_option("--hadd", dest = "hadd", action = "store_true")
     (options, args) = parser.parse_args()
     
-    folder = "/nfs/dust/cms/user/kutznerv/shorttrack/analysis/eventselection/tools/skim_69_merged"
+    folder = "/nfs/dust/cms/user/kutznerv/shorttrack/analysis/eventselection/tools/skim_01_merged"
    
     event_selections = {
                 "Baseline":               "(n_goodleptons==0 || (tracks_invmass>110 && leadinglepton_mt>90))",
@@ -128,19 +139,23 @@ if __name__ == "__main__":
                 "BaselineMuons":          "n_goodelectrons==0 && n_goodmuons>=1 && tracks_invmass>110 && leadinglepton_mt>90",
                 "HadBaseline":            "HT>150 && MHT>150 && n_goodjets>=1 && (n_goodleptons==0 || (tracks_invmass>110 && leadinglepton_mt>90))",
                 "SMuBaseline":            "HT>150 && n_goodjets>=1 && n_goodmuons>=1 && n_goodelectrons==0 && tracks_invmass>110 && leadinglepton_mt>90",
-                "SMuValidationZLL":       "n_goodjets>=1 && n_goodmuons>=1 && n_goodelectrons==0 && tracks_invmass>65 && tracks_invmass<110 && leptons_mt>90",
+                "SMuValidationZLL":       "n_goodjets>=1 && n_goodmuons>=1 && n_goodelectrons==0 && tracks_invmass>65 && tracks_invmass<110 && leadinglepton_mt>90",
                 "SElBaseline":            "HT>150 && n_goodjets>=1 && n_goodelectrons>=1 && n_goodmuons==0 && tracks_invmass>110 && leadinglepton_mt>90",
-                "SElValidationZLL":       "n_goodjets>=1 && n_goodelectrons>=1 && n_goodmuons==0 && tracks_invmass>65 && tracks_invmass<110 && leptons_mt>90",
-                "SElValidationMT":        "n_goodjets>=1 && n_goodelectrons==1 && n_goodmuons==0 && leptons_mt<70",
-                "SMuValidationMT":        "n_goodjets>=1 && n_goodmuons==1 && n_goodelectrons==0 && leptons_mt<70",
+                "SElValidationZLL":       "n_goodjets>=1 && n_goodelectrons>=1 && n_goodmuons==0 && tracks_invmass>65 && tracks_invmass<110 && leadinglepton_mt>90",
+                "SElValidationMT":        "n_goodjets>=1 && n_goodelectrons==1 && n_goodmuons==0 && leadinglepton_mt<70",
+                "SMuValidationMT":        "n_goodjets>=1 && n_goodmuons==1 && n_goodelectrons==0 && leadinglepton_mt<70",
                 "FakeRateDetQCDJetHT":    "MHT<200",
                       }
 
     regions = collections.OrderedDict()
-    regions["SElValidationMT"] = ["leptons_mt", "tracks_invmass", "HT", "MHT", "DeDxAverageCorrected", "n_goodjets", "n_btags", "MinDeltaPhiMhtJets", "region"]
-    regions["SElValidationZLL"] = regions["SElValidationMT"]
-    regions["SMuValidationMT"] = regions["SElValidationMT"]
-    regions["SMuValidationZLL"] = regions["SElValidationMT"]
+
+    #allvars = ["leadinglepton_mt", "tracks_invmass", "HT", "MHT", "tracks_deDxHarmonic2pixel", "n_goodjets", "n_btags", "MinDeltaPhiMhtJets", "region", "region_sideband"]
+    allvars = ["leadinglepton_mt", "tracks_invmass"]
+
+    regions["SElValidationMT"] = allvars
+    regions["SElValidationZLL"] = allvars
+    regions["SMuValidationMT"] = allvars
+    regions["SMuValidationZLL"] = allvars
     #regions["FakeRateDetQCDJetHT"] = ["HT", "n_allvertices"]
     #regions["Baseline"] = regions["SElValidationMT"]
     #regions["HadBaseline"] = regions["SElValidationMT"]
@@ -158,7 +173,7 @@ if __name__ == "__main__":
                 scaling = histos[h_suffix][1]
 
                 if "FakeRateDetQCDJetHT" in region:
-                    if "_fakecr_" in h_suffix or "_sr_" in h_suffix:
+                    if "_fakecr" in h_suffix or "_sr" in h_suffix:
                         parameters.append([variable, event_selections[region] + cuts, scaling, region + "_Summer16QCD", h_suffix, folder, ["Summer16.QCD"], output_root_file])
                         parameters.append([variable, event_selections[region] + cuts, scaling, region + "_Run2016JetHT", h_suffix, folder, ["Run2016*JetHT"], output_root_file])
                         continue
@@ -175,7 +190,11 @@ if __name__ == "__main__":
     # run script:
 
     if options.index:
-        write_histogram_to_file(*parameters[int(options.index)])
+
+        if not os.path.exists(output_root_file):
+            write_histogram_to_file(*parameters[int(options.index)])
+        else:
+            print "Already done"
 
     elif options.hadd:
         os.system("hadd -f %s.root %s/ddbg_*.root" % (options.output_folder, options.output_folder))
@@ -185,8 +204,18 @@ if __name__ == "__main__":
         import __main__ as main
         this_scripts_name = main.__file__
 
-        commands = []
-        for i_parameter in range(len(parameters)):
-            commands.append("%s --index %s --folder %s" % (this_scripts_name, i_parameter, options.output_folder))
-        GridEngineTools.runParallel(commands, "grid", "%s.condor" % this_scripts_name.split("/")[-1].split(".")[0], use_more_time=15000)
+        def chunks(lst, n):
+                    """Yield successive n-sized chunks from lst."""
+                    for i in range(0, len(lst), n):
+                        yield lst[i:i + n]
+
+        commands = []      
+        chunks_of_parameters = chunks(range(len(parameters)), int(options.plots_per_job))
+        for chunks_of_parameter in chunks_of_parameters:
+            subcommand = ""
+            for param_index in chunks_of_parameter:
+                subcommand += "%s --index %s --folder %s; " % (this_scripts_name, param_index, options.output_folder)
+            commands.append(subcommand)
+
+        GridEngineTools.runParallel(commands, "grid", "%s.condor" % options.output_folder, use_more_time=15000)
 
--- analyzer/plot_backgrounds.py
+++ analyzer/plot_backgrounds.py
@@ -6,7 +6,6 @@ import collections
 import glob
 import os
 import shared_utils
-import get_backgrounds
 
 def plot_validation(variable, root_file, mclabel, datalabel, category, lumi, region, pdffile):
     
@@ -14,7 +13,10 @@ def plot_validation(variable, root_file, mclabel, datalabel, category, lumi, reg
     histos = collections.OrderedDict()
     fin = TFile(root_file, "read")
 
+    import get_backgrounds
     zones = get_backgrounds.histos.keys()
+    #zones = ['_sr_short', '_sr_long', '_srSideband_short', '_srSideband_long', '_srMid_short', '_srMid_long', '_srMidHighDeDx_short', '_srMidHighDeDx_long', '_srHighDeDx_short', '_srHighDeDx_long', '_fakecr_short', '_fakecr_long', '_fakecrSideband_short', '_fakecrSideband_long', '_fakecrMid_short', '_fakecrMid_long', '_fakecrMidHighDeDx_short', '_fakecrMidHighDeDx_long', '_fakecrHighDeDx_short', '_fakecrHighDeDx_long', '_fakeprediction_short', '_fakeprediction_long', '_fakepredictionSideband_short', '_fakepredictionSideband_long', '_fakepredictionMid_short', '_fakepredictionMid_long', '_fakepredictionMidHighDeDx_short', '_fakepredictionMidHighDeDx_long', '_fakepredictionHighDeDx_short', '_fakepredictionHighDeDx_long', '_lowDeDxPromptElectron', '_lowDeDxDTnoLep', '_lowDeDxDT', '_midHighDeDxPromptElectron', '_midHighDeDxDTnoLep', '_midHighDeDxDT', '_midDeDxPromptElectron', '_midDeDxDTnoLep', '_midDeDxDT', '_highDeDxPromptElectron', '_highDeDxDTnoLep', '_highDeDxDT', '_srgenfakes_short', '_srgenfakes_long', '_srgenprompt_short', '_srgenprompt_long']
+
     for zone in zones:
         histos[mclabel + zone] = fin.Get(variable + "_" + region + "_" + mclabel + zone)
         histos[datalabel + zone] = fin.Get(variable + "_" + region + "_" + datalabel + zone)
@@ -42,25 +44,26 @@ def plot_validation(variable, root_file, mclabel, datalabel, category, lumi, reg
                 del histos[label.replace("_short", "")]
                 continue
 
+    fin.Close()
+
     for label in histos.keys():
         easyname = label.replace(datalabel, "Data")
         easyname = easyname.replace(mclabel, "MC")
         easyname = easyname.replace("_", " ")
         easyname = easyname.replace("sr", "SR")
         histos[label].SetTitle(easyname)
-
-    fin.Close()
     
     # ABCD method to get prompt contribution in SR:
 
     # first, subtract fake background from low-dE/dx sideband DT region:
-    histos[datalabel + "_lowDeDxDT_fakesubtraced"] = histos[datalabel + "_lowDeDxDT"].Clone()
-    histos[datalabel + "_lowDeDxDT_fakesubtraced"].Add(histos[datalabel + "_fakepredictionSideband"], -1)
+    #histos[datalabel + "_lowDeDxDT_fakesubtraced"] = histos[datalabel + "_lowDeDxDT"].Clone()
+    #histos[datalabel + "_lowDeDxDT_fakesubtraced"].Add(histos[datalabel + "_fakepredictionSideband"], -1)
     
-    histos[datalabel + "_promptprediction"] = histos[datalabel + "_highDeDxPromptElectron"].Clone()
-    #histos[datalabel + "_promptprediction"].Multiply(histos[datalabel + "_lowDeDxDT"])
-    histos[datalabel + "_promptprediction"].Multiply(histos[datalabel + "_lowDeDxDT_fakesubtraced"])
-    histos[datalabel + "_promptprediction"].Divide(histos[datalabel + "_lowDeDxPromptElectron"])
+    # ABCD method: C = D  * A / B
+    histos[datalabel + "_promptprediction"] = histos[datalabel + "_midDeDxPromptElectron"].Clone()
+    histos[datalabel + "_promptprediction"].Multiply(histos[datalabel + "_midDeDxDT"])
+    #histos[datalabel + "_promptprediction"].Multiply(histos[datalabel + "_lowDeDxDT_fakesubtraced"])
+    histos[datalabel + "_promptprediction"].Divide(histos[datalabel + "_midDeDxPromptElectron"])
 
     # plot:
     canvas = shared_utils.mkcanvas()
@@ -69,8 +72,29 @@ def plot_validation(variable, root_file, mclabel, datalabel, category, lumi, reg
     legend.SetHeader(region + " region")
     colors = [kBlack, kRed, kBlue, kGreen, kOrange, kAzure, kMagenta, kYellow, kTeal]
     
+    # Fake prediction using FR map
     histos[datalabel + "_fakeprediction" + category].SetFillColor(216)
     histos[datalabel + "_fakeprediction" + category].SetTitle("Fake prediction")
+    
+    # Fake pred. using constant factor:
+    #data_fakerate_short = fin.Get("HT_FakeRateDetQCDJetHT_Run2016JetHT_sr_short")
+    #data_fakerate_short_denom = fin.Get("HT_FakeRateDetQCDJetHT_Run2016JetHT_fakecr_short")
+    #data_fakerate_short.SetDirectory(0)
+    #data_fakerate_short_denom.SetDirectory(0)
+    #fr_short = data_fakerate_short.Integral()/data_fakerate_short_denom.Integral()
+    #data_fakerate_short.Divide(data_fakerate_short_denom)
+    #data_fakerate_short.Draw()
+    #canvas.Print("fakerate_data_short.pdf")
+    #
+    #data_fakerate_long = fin.Get("HT_FakeRateDetQCDJetHT_Run2016JetHT_sr_long")
+    #data_fakerate_long_denom = fin.Get("HT_FakeRateDetQCDJetHT_Run2016JetHT_fakecr_long")
+    #data_fakerate_long.SetDirectory(0)
+    #data_fakerate_long_denom.SetDirectory(0)
+    #fr_long = data_fakerate_long.Integral()/data_fakerate_long_denom.Integral()
+    #data_fakerate_long.Divide(data_fakerate_long_denom)
+    #data_fakerate_long.Draw()
+    #canvas.Print("fakerate_data_long.pdf")    
+    
     histos[datalabel + "_promptprediction"].SetFillColor(207)
     histos[datalabel + "_promptprediction"].SetTitle("Prompt prediction")
 
@@ -81,15 +105,21 @@ def plot_validation(variable, root_file, mclabel, datalabel, category, lumi, reg
 
     lumi = float("%.2f" % (lumi/1e3))
 
-    hratio, pad1, pad2 = shared_utils.FabDraw(canvas, legend, histos[datalabel + "_srMidHighDeDx" + category], stacked_histograms, lumi = lumi, datamc = 'Data')
+    if "Validation" in region:
+        datahist = histos[datalabel + "_sr" + category]
+    else:
+        datahist = stacked_histograms[-1]
+    
+    hratio, pad1, pad2 = shared_utils.FabDraw(canvas, legend, datahist, stacked_histograms, lumi = lumi, datamc = 'Data')
+    stacked_histograms[-1].SetTitle("")
 
-    if "ValidationMT" in region and variable == "leptons_mt":
+    if "ValidationMT" in region and variable == "leadinglepton_mt":
         ymin = 5e1; ymax = 2e4
     if "ValidationMT" in region and variable == "tracks_invmass":
         ymin = 1e-1; ymax = 5e3
     elif "ValidationZLL" in region and variable == "tracks_invmass":
         ymin = 5e-2; ymax = 5e3
-    elif "ValidationZLL" in region and variable == "leptons_mt":
+    elif "ValidationZLL" in region and variable == "leadinglepton_mt":
         ymin = 1e-1; ymax = 1e4
     else:
         ymin = 5e1; ymax = 2e4
@@ -104,15 +134,25 @@ def plot_validation(variable, root_file, mclabel, datalabel, category, lumi, reg
     xlabel = xlabel.replace("leptons_mt", "m_{T}^{lepton} (GeV)")
     hratio.GetXaxis().SetTitle(xlabel)
 
+    # add fake CR:
     histos[datalabel + "_fakecr" + category].SetLineColor(kTeal)
     histos[datalabel + "_fakecr" + category].Draw("same")
     legend.AddEntry(histos[datalabel + "_fakecr" + category], "Fake CR")
 
+    # add prompt CR:
     histos[datalabel + "_highDeDxPromptElectron"].SetLineColor(kTeal+2)
     histos[datalabel + "_highDeDxPromptElectron"].Draw("same")
     legend.AddEntry(histos[datalabel + "_highDeDxPromptElectron"], "Prompt el. (dE/dx>2.1)")
 
-
+    ## add MC Truth info:
+    #histos[mclabel + "_srgenfakes" + category].SetLineColor(kRed)
+    #histos[mclabel + "_srgenfakes" + category].Draw("same")
+    #legend.AddEntry(histos[mclabel + "_srgenfakes" + category], "Genfakes")
+    #
+    #histos[mclabel + "_srgenprompt" + category].SetLineColor(kOrange)
+    #histos[mclabel + "_srgenprompt" + category].Draw("same")
+    #legend.AddEntry(histos[mclabel + "_srgenprompt" + category], "Genprompt")
+    
     for ibin in range(1,hratio.GetXaxis().GetNbins()+1):
         if hratio.GetBinContent(ibin)==0:
             hratio.SetBinContent(ibin,-999)
@@ -125,17 +165,17 @@ if __name__ == "__main__":
     gStyle.SetOptStat(0)
     TH1D.SetDefaultSumw2()
    
-    folder = "../skims/current"
+    folder = "/nfs/dust/cms/user/kutznerv/shorttrack/analysis/eventselection/tools/skim_69_merged"
+    inputfile = "ddbg5.root"
+
     with open(folder + "/luminosity.py") as fin:
         lumis = eval(fin.read())
 
-    inputfile = "ddbg2.root"
-
     regions = collections.OrderedDict()
-    regions["SElValidationMT"] = ["leptons_mt", "tracks_invmass"]
-    regions["SMuValidationMT"] = ["leptons_mt", "tracks_invmass"]
-    regions["SElValidationZLL"] = ["leptons_mt", "tracks_invmass"]
-    regions["SMuValidationZLL"] = ["leptons_mt", "tracks_invmass"]
+    regions["SElValidationMT"] = ["leadinglepton_mt", "tracks_invmass"]
+    #regions["SMuValidationMT"] = ["leadinglepton_mt", "tracks_invmass"]
+    regions["SElValidationZLL"] = ["leadinglepton_mt", "tracks_invmass"]
+    #regions["SMuValidationZLL"] = ["leadinglepton_mt", "tracks_invmass"]
     
     for region in regions:
         for variable in regions[region]:
@@ -147,7 +187,7 @@ if __name__ == "__main__":
             else:
                 continue
             
-            plot_validation(variable, inputfile, "Summer16", "Run2016%s" % datalabel, "", lumis["Run2016_%s" % datalabel] * 1e3, region, variable + "_" + region)
-
-    
+            #plot_validation(variable, inputfile, "Summer16", "Run2016%s" % datalabel, "_short", lumis["Run2016_%s" % datalabel] * 1e3, region, "short_" + variable + "_" + region)
+            #plot_validation(variable, inputfile, "Summer16", "Run2016%s" % datalabel, "_long", lumis["Run2016_%s" % datalabel] * 1e3, region, "long_" + variable + "_" + region)
+            plot_validation(variable, inputfile, "Summer16", "Run2016%s" % datalabel, "", lumis["Run2016_%s" % datalabel] * 1e3, region, "combined_" + variable + "_" + region)
 
