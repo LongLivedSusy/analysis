631915561c01b506aa6a9ae59f2344c6b2555dec
Sang Il Pak <spak@cern.ch>
2020-04-29 09:45:35 +0200

    delete

--- dedx/work/remove_duplicateline.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-lines_seen = set() # holds lines already seen
-outfile = open("out.txt", "w")
-for line in open("Summer16.QCD.txt", "r"):
-    if line not in lines_seen: # not a duplicate
-        outfile.write(line)
-        lines_seen.add(line)
-outfile.close()
