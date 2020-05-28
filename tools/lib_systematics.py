394cfe7baadbe4d0433d84800ffcd29e1a81e4ff
Sang Il Pak <spak@cern.ch>
2020-05-28 10:46:32 +0200

    update

--- tools/lib_systematics.py
+++ tools/lib_systematics.py
@@ -1,8 +1,21 @@
 import os, sys
 from ROOT import *
 
-def get_btag_weight(tree,nSigmaBtagSF,nSigmaBtagFastSimSF,isFastSim):
+def read_sigmas(sigmas_txt):
+    sigmas_dict = {}
+
+    with open (sigmas_txt, 'r') as fsyst:
+	while True :
+	    line = fsyst.readline().strip()
+	    if not line : break
+	    if line.startswith('#') : continue #To ignore commented line in txt file
+	    line = line.split()
+	    
+	    sigmas_dict[line[0]] = [int(line[1]), int(line[2]), int(line[3]), int(line[4])]
     
+    return sigmas_dict
+
+def prepareReaderBtagSF():
     # load b tag sf from csv file
     import ROOT
     
@@ -46,7 +59,11 @@ def get_btag_weight(tree,nSigmaBtagSF,nSigmaBtagFastSimSF,isFastSim):
         2,          # 0 is for b flavour, 1: FLAV_C, 2: FLAV_UDSG 
         "incl"      # measurement type
     )
+
+    return readerBtag
     
+
+def get_btag_weight(tree,nSigmaBtagSF,nSigmaBtagFastSimSF,isFastSim,readerBtag):
     
     ## Get Btagging efficiency map for signal sample
     if 'T1qqqq' in tree.GetFile().GetName() : 
@@ -64,55 +81,53 @@ def get_btag_weight(tree,nSigmaBtagSF,nSigmaBtagFastSimSF,isFastSim):
     
     # jet loop start here
     for ijet, jet in enumerate(tree.Jets):
+	if not tree.Jets_ID[ijet] : continue
 	if not (abs(jet.Eta())<2.4 and jet.Pt()>30): continue
 	
-	if jet.Pt() >= 1000 : JetPt = 999.
-	else : JetPt = jet.Pt()
-       
         eff = 1.0
 	# b tag efficiency
 	if tree.Jets_hadronFlavor[ijet]== 5: # truth b particle
 	    heff = fbeff.Get("efficiency_b")
-	    binx = heff.GetXaxis().FindBin(JetPt)
+	    binx = heff.GetXaxis().FindBin(jet.Pt())
 	    biny = heff.GetYaxis().FindBin(jet.Eta())
 	    eff = heff.GetBinContent(binx,biny)
 	    FLAV = 0
-	    #print 'b jetpt : ', JetPt, "jeteta:",jet.Eta()," binx:",binx,", biny:",biny,"eff:",eff
+	    #print 'b jetpt : ', jet.Pt(), "jeteta:",jet.Eta()," binx:",binx,", biny:",biny,"eff:",eff
         elif tree.Jets_hadronFlavor[ijet]== 4: # truth c particle
 	    heff = fbeff.Get("efficiency_c")
-	    binx = heff.GetXaxis().FindBin(JetPt)
+	    binx = heff.GetXaxis().FindBin(jet.Pt())
             biny = heff.GetYaxis().FindBin(jet.Eta())
 	    eff = heff.GetBinContent(binx,biny)
 	    FLAV = 1
-	    #print 'c jetpt : ', JetPt, "jeteta:",jet.Eta()," binx:",binx,", biny:",biny,"eff:",eff
+	    #print 'c jetpt : ', jet.Pt(), "jeteta:",jet.Eta()," binx:",binx,", biny:",biny,"eff:",eff
 	elif tree.Jets_hadronFlavor[ijet]== 0: # truth udsg particle
 	    heff = fbeff.Get("efficiency_udsg")
-	    binx = heff.GetXaxis().FindBin(JetPt)
+	    binx = heff.GetXaxis().FindBin(jet.Pt())
 	    biny = heff.GetYaxis().FindBin(jet.Eta())
 	    eff = heff.GetBinContent(binx,biny)
 	    FLAV = 2
-	    #print 'udsg jetpt : ', JetPt, "jeteta:",jet.Eta()," binx:",binx,", biny:",biny,"eff:",eff
+	    #print 'udsg jetpt : ', jet.Pt(), "jeteta:",jet.Eta()," binx:",binx,", biny:",biny,"eff:",eff
 	else : print 'wired Jets_hadronFlavor : ', tree.Jets_hadronFlavor[ijet]
-	    
+	   
 	sf_cen = readerBtag.eval_auto_bounds(
 	    'central',      # systematic (here also 'up'/'down' possible)
 	    FLAV,              # jet flavor
 	    abs(jet.Eta()),      # absolute value of eta
-	    JetPt        # pt
+	    jet.Pt()        # pt
 	)
 	sf_up = readerBtag.eval_auto_bounds(
 	    'up',           # systematic (here also 'up'/'down' possible)
 	    FLAV,              # jet flavor
 	    abs(jet.Eta()),      # absolute value of eta
-	    JetPt        # pt
+	    jet.Pt()        # pt
 	)
 	sf_down = readerBtag.eval_auto_bounds(
 	    'down',         # systematic (here also 'up'/'down' possible)
 	    FLAV,              # jet flavor
 	    abs(jet.Eta()),      # absolute value of eta
-	    JetPt        # pt
+	    jet.Pt()        # pt
 	)
-	#print '%sth jet pt : %.2f, eta : %.2f, flavor : %s, sf_cen : %.2f, sf_up : %.2f, sf_down : %.2f'%(ijet,JetPt,jet.Eta(),tree.Jets_hadronFlavor[ijet],sf_cen,sf_up,sf_down)
+	#print '%sth jet pt : %.2f, eta : %.2f, flavor : %s, sf_cen : %.2f, sf_up : %.2f, sf_down : %.2f'%(ijet,jet.Pt(),jet.Eta(),tree.Jets_hadronFlavor[ijet],sf_cen,sf_up,sf_down)
 	
 	sf = get_syst(sf_cen, sf_up, sf_down, nSigmaBtagSF)
 
@@ -164,9 +179,10 @@ def get_isr_weight(tree,nSigmaISR):
     #d = 1.0	# Before determine D value
     #d = 1.121	# T2tt
     if 'g1800_chi1400' in fname : d = 1.15598 # g1800_chi1400
-    else : d = 1
+    elif 'T1qqqq' in fname : d = 1.123 # T1qqqq
+    else : d = 1.0
     n = tree.NJetsISR
-    w_nom = 0
+    w_nom = 0.0
     
     if   n==0 : w_nom = d
     elif n==1 : w_nom = d * 0.920
@@ -174,7 +190,8 @@ def get_isr_weight(tree,nSigmaISR):
     elif n==3 : w_nom = d * 0.715
     elif n==4 : w_nom = d * 0.662
     elif n==5 : w_nom = d * 0.561
-    else      : w_nom = d * 0.511
+    elif n>=6 : w_nom = d * 0.511
+    else : print 'what is n?' 
     err = (1-w_nom)/2
     w_isr_up   = w_nom + err
     w_isr      = w_nom
