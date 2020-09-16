efdf178d6c0587261236fb7d869811a84224631f
Viktor Kutzner <viktor.kutzner@cern.ch>
2020-09-16 17:26:58 +0200

    reorganized BDT

--- disappearing-track-tag/2016-short-tracks/runTMVA.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "running TMVA..."
-
-g++ -w -g -Wall -Wextra -lTMVA -lTMVAGui `root-config --cflags --libs` tmva.cxx -o tmva
-
-./tmva /nfs/dust/cms/user/kutznerv/DisappTrksNtupleSidecarI7d/tracks-short/signal.root output.root /nfs/dust/cms/user/kutznerv/DisappTrksNtupleSidecarI7d/tracks-short -1
