c9fcf472419f0d830b74622341fbc299c8b4e7f5
Sang Il Pak <spak@cern.ch>
2020-09-28 08:13:41 +0200

    update

--- dedx/submit_analyzer.py
+++ dedx/submit_analyzer.py
@@ -4,7 +4,7 @@ from glob import glob
 from GridEngineTools import runParallel
 import argparse
 
-def do_submission(commands, output_folder, condorDir = "condor", executable = "analyzer_dEdx.py", runmode = "grid", dontCheckOnJobs=True, confirm=True):
+def do_submission(commands, output_folder, condorDir = "condor", executable = "analyzer_leptontrack.py", runmode = "grid", dontCheckOnJobs=True, confirm=True):
 
     print "Submitting \033[1m%s jobs\033[0m, output folder will be \033[1m%s\033[0m." % (len(commands), output_folder)
     os.system("mkdir -p %s" % output_folder)
@@ -24,16 +24,21 @@ if __name__ == "__main__":
     #Inputfile txt path
     path = "./inputs/"
     #inputfiles = sorted(glob(path+'/*.txt'))
-    #inputfiles = sorted(glob(path+'/Run2016*.txt'))
+    inputfiles = sorted(glob(path+'/Run2016*.txt'))
     #inputfiles = sorted(glob(path+'/Run2017*.txt'))
     #inputfiles = sorted(glob(path+'/Summer16*.txt'))
     #inputfiles = sorted(glob(path+'/RunIIFall17*.txt'))
+    #inputfiles = sorted(glob(path+'/RunIIFall17*.txt')+glob(path+'/Run2017*.txt'))
     #inputfiles = ["./inputs/Run2016H-SingleMuon.txt"]
-    inputfiles = ["./inputs/Run2017B-SingleMuon.txt"]
+    #inputfiles = ["./inputs/Run2016H-SingleElectron.txt"]
+    #inputfiles = ["./inputs/Run2017B-SingleMuon.txt"]
+    #inputfiles = ["./inputs/Run2018D-EGamma.txt"]
     #inputfiles = ["./inputs/Summer16.WJetsToLNu_HT-100To200.txt"]
-    #inputfiles = ["./inputs/RunIIFall17MiniAODv2.DYJetsToLL_M-50_HT-600to800.txt"]
-    #inputfiles = ["./inputs/RunIISummer16MiniAODv3.SMS-T2bt-LLChipm_ctau-200_mLSP-900_.txt"]
-    
+    #inputfiles = ["./inputs/Summer16.DYJetsToLL_M-50_TuneCUETP8M1.txt"]
+    #inputfiles = ["./inputs/RunIIFall17MiniAODv2.DYJetsToLL_M-50_TuneCP5.txt"]
+    #inputfiles = ["./inputs/RunIISummer16MiniAODv3.SMS-T2bt-LLChipm_ctau-200_mLSP-900_TuneCUETP8M1.txt"]
+   
+    condorDir = 'condor_data'
     output_dir = "./output_smallchunks/"
     if not os.path.exists(output_dir):
 	os.system("mkdir -p "+output_dir)
@@ -43,12 +48,14 @@ if __name__ == "__main__":
     # Number of split per inputfile
     split = True
     nsplit = 100
+    isfast = False
 
     commands=[]
     lines=[]
     for inputfile in sorted(inputfiles):
 	cnt = 0
         if "SMS" in inputfile : split = False
+        if "FastSim" in inputfile : isfast = True
 	with open(inputfile) as f:
 	    lines = f.readlines()
 	    if split : 
@@ -56,21 +63,21 @@ if __name__ == "__main__":
 		for i,chunk in enumerate(input_chunks):
 		    output = inputfile.split('/')[-1].replace('.txt','_'+str(i)+'.root')
 		    chunk = str(chunk).replace('\\n','').replace(", "," ").replace("[","").replace("]","")
-		    command = "python analyzer_dEdx.py --input {} --output_dir {} --output {} ".format(chunk, output_dir, output)
+		    command = "python analyzer_leptontrack.py --input {} --output_dir {} --output {} ".format(chunk, output_dir, output)
+		    if isfast : 
+			command += "--fast"
         	    commands.append(command)
 
 	    elif not split : 
-		output = inputfile.split('/')[-1].replace('_.txt','.root')
+		output = inputfile.split('/')[-1].replace('.txt','.root')
 		chunk = str(lines).replace('\\n','').replace(", "," ").replace("[","").replace("]","")
-		command = "python analyzer_dEdx.py --input {} --output_dir {} --output {} ".format(chunk, output_dir, output)
+		command = "python analyzer_leptontrack.py --input {} --output_dir {} --output {} ".format(chunk, output_dir, output)
+		if isfast :
+		    command += " --fast"
         	commands.append(command)
 	    else : 
 		print('Something wrong in splitting')
 		quit()
-
-    if test : 
-	os.system("mkdir -p output_smallchunks_test")
-	print('python analyzer_dEdx.py --input {} --output_dir {} --output {} --nev 10000;'.format(chunk, "output_smallchunks_test", output))
-	os.system('python analyzer_dEdx.py --input {} --output_dir {} --output {} --nev 10000;'.format(chunk, "output_smallchunks_test", output))
-    else :
-	do_submission(commands, output_dir, condorDir='condor_2017')
+    
+    # Submit
+    do_submission(commands, output_dir, condorDir=condorDir)
