e80fe8437494d56be629cac158ba3e14eda10805
Viktor Kutzner <viktor.kutzner@cern.ch>
2020-10-31 23:25:06 +0100

    added short track BDTs with pt>10

---
+++ disappearing-track-tag/2017-short-tracks-may20-dxy-chi2-v2-pt10/run_on_grid.py
@@ -0,0 +1,6 @@
+#!/bin/env python
+from GridEngineTools import runParallel
+import os
+
+os.system("cp ../trainBDT_template.py trainBDT.py; chmod +x trainBDT.py")
+runParallel(["./trainBDT.py --category short --use_chi2 --dxyinformed --phase 1 --path /afs/desy.de/user/k/kutznerv/dust/shorttrack/analysis/ntupleanalyzer/skim_55_pt10_pixelonly_merged"], "grid", confirm=False, babysit=False)
