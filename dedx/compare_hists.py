b98ae83b710ccfef903c0a355794d992b16c3b56
Sang Il Pak <spak@cern.ch>
2020-11-16 08:17:20 +0100

    rename

--- dedx/compare_hists.py
+++ dedx/compare_hists.py
@@ -1,11 +1,52 @@
 import os,sys
-from ROOT import *
+#from ROOT import *
 from glob import glob
-#from natsort import natsorted,ns
+import ROOT as rt
+import CMS_lumi, tdrstyle
+import array
 
-gROOT.SetBatch(1)
-gStyle.SetOptStat(False)
-#gStyle.SetOptFit(1111)
+#set the tdr style
+tdrstyle.setTDRStyle()
+
+#change the CMS_lumi variables (see CMS_lumi.py)
+CMS_lumi.lumi_7TeV = "4.8 fb^{-1}"
+CMS_lumi.lumi_8TeV = "18.3 fb^{-1}"
+CMS_lumi.writeExtraText = 1
+CMS_lumi.extraText = "Preliminary"
+CMS_lumi.lumi_sqrtS = "13 TeV" # used with iPeriod = 0, e.g. for simulation-only plots (default is an empty string)
+
+iPos = 11
+if( iPos==0 ): CMS_lumi.relPosX = 0.12
+
+H_ref = 600; 
+W_ref = 800; 
+W = W_ref
+H  = H_ref
+
+# 
+# Simple example of macro: plot with CMS name and lumi text
+#  (this script does not pretend to work in all configurations)
+# iPeriod = 1*(0/1 7 TeV) + 2*(0/1 8 TeV)  + 4*(0/1 13 TeV) 
+# For instance: 
+#               iPeriod = 3 means: 7 TeV + 8 TeV
+#               iPeriod = 7 means: 7 TeV + 8 TeV + 13 TeV 
+#               iPeriod = 0 means: free form (uses lumi_sqrtS)
+# Initiated by: Gautier Hamel de Monchenault (Saclay)
+# Translated in Python by: Joshua Hardenbrook (Princeton)
+# Updated by:   Dinko Ferencek (Rutgers)
+#
+
+iPeriod = 3
+
+# references for T, B, L, R
+T = 0.08*H_ref
+B = 0.12*H_ref
+L = 0.12*W_ref
+R = 0.04*W_ref
+
+rt.gROOT.SetBatch(1)
+rt.gStyle.SetOptStat(False)
+#rt.gStyle.SetOptFit(1111)
 
 #format_c = 'pdf'
 format_c = 'png'
@@ -14,91 +55,143 @@ format_c = 'png'
 #outputdir = './plots/T2bt_mStop1300_mLSP50'
 #outputdir = './plots/T2bt_mStop1300_mLSP200'
 #outputdir = './plots/T2bt_mStop1300_mLSP1100'
-outputdir = './plots/T2bt_mStop1300_mLSP1100_dEdx'
+#outputdir = './plots/T2bt_mStop1300_mLSP1100_dEdx'
+outputdir = './plots/T1qqqq_mStop1300_mLSP1100_dEdx'
 
-rebin = 1
+#rebin = 1
 #rebin = 5
-#rebin = 10
+rebin = 10
 #rebin = 20
+    
+if not os.path.exists(outputdir) : os.system('mkdir -p '+outputdir)
+    
+f1 = rt.TFile('./output_test/RunIIFall17MiniAODv2.FastSim-SMS-T1qqqq-LLChipm_ctau-200_TuneCP2_13TeV-madgraphMLM-pythia8_mGluino2200_mLSP2000.root')
+f2 = rt.TFile('./output_smallchunks_localrun/T2bt_mStop1300_mLSP1100/RunIISummer16MiniAODv3.SMS-T2bt-LLChipm_ctau-200_mLSP-1100_TuneCUETP8M1.root')
 
-dict_Summer16_FullSimSignal = {
-	#'Summer16FullSim.SMS-T2bt-LLChipm_ctau-200_mLSP-1':'./output_smallchunks_localrun/T2bt_mStop1300_mLSP1/RunIISummer16MiniAODv3.SMS-T2bt-LLChipm_ctau-200_mLSP-1_TuneCUETP8M1.root',
-	#'Summer16FullSim.SMS-T2bt-LLChipm_ctau-200_mLSP-50':'./output_smallchunks_localrun/T2bt_mStop1300_mLSP50/RunIISummer16MiniAODv3.SMS-T2bt-LLChipm_ctau-200_mLSP-50_TuneCUETP8M1.root',
-	#'Summer16FullSim.SMS-T2bt-LLChipm_ctau-200_mLSP-200':'./output_smallchunks_localrun/T2bt_mStop1300_mLSP200/RunIISummer16MiniAODv3.SMS-T2bt-LLChipm_ctau-200_mLSP-200_TuneCUETP8M1.root',
-	'Summer16FullSim.SMS-T2bt-LLChipm_ctau-200_mLSP-1100':'./output_smallchunks_localrun/T2bt_mStop1300_mLSP1100/RunIISummer16MiniAODv3.SMS-T2bt-LLChipm_ctau-200_mLSP-1100_TuneCUETP8M1.root',
-	#'Summer16FullSim.SMS-T2bt-LLChipm_ctau-200_mLSP-900':'./output_chargino/RunIISummer16MiniAODv3.SMS-T2bt-LLChipm_ctau-200_mLSP-900_TuneCUETP8M1.root',
-	#'Summer16FullSim.SMS-T2bt-LLChipm_ctau-200_mLSP-1100':'./output_chargino/RunIISummer16MiniAODv3.SMS-T2bt-LLChipm_ctau-200_mLSP-1100_TuneCUETP8M1.root',
-        }
+hists=[
+    #'hGenGluino',
+    #'hGenStopMass',
+    'hGenLSPMass',
+    'hGenCharginoP',
+    'hGenCharginoPt',
+    'hGenCharginoEta',
+    'hGenCharginoPhi',
+	
+    'hTrkP_charginomatch',
+    'hTrkPt_charginomatch',
+    'hTrkEta_charginomatch',
+    'hTrkPhi_charginomatch',
 
-dict_Summer16_FastSimSignal = {
-	#'Summer16FastSim.SMS-T2bt-LLChipm_ctau-200_mStop-1300_mLSP-1':'./output_smallchunks_localrun/T2bt_mStop1300_mLSP1/Summer16PrivateFastSim.SMS-T2bt-LLChipm_ctau-200_mStop-1300_mLSP-1to200.root',
-	#'Summer16FastSim.SMS-T2bt-LLChipm_ctau-200_mStop-1300_mLSP-50':'./output_smallchunks_localrun/T2bt_mStop1300_mLSP50/Summer16PrivateFastSim.SMS-T2bt-LLChipm_ctau-200_mStop-1300_mLSP-1to200.root',
-	#'Summer16FastSim.SMS-T2bt-LLChipm_ctau-200_mStop-1300_mLSP-200':'./output_smallchunks_localrun/T2bt_mStop1300_mLSP200/Summer16PrivateFastSim.SMS-T2bt-LLChipm_ctau-200_mStop-1300_mLSP-1to200.root',
-	'Summer16FastSim.SMS-T2bt-LLChipm_ctau-200_mStop-1300_mLSP-1100':'./output_smallchunks_localrun/T2bt_mStop1300_mLSP1100/Summer16PrivateFastSim.SMS-T2bt-LLChipm_ctau-200_mStop-1300_mLSP-1100and300.root',
-	#'Summer16FastSim.SMS-T2bt-LLChipm_ctau-200_mLSP-900':'./output_chargino/Summer16PrivateFastSim.SMS-T2bt-LLChipm_ctau-200_mLSP-900_TuneCUETP8M1.root',
-	#'Summer16FastSim.SMS-T2bt-LLChipm_ctau-200_mStop-1300_mLSP-1100':'./output_chargino/Summer16PrivateFastSim.SMS-T2bt-LLChipm_ctau-200_mStop-1300_mLSP-1100and300.root',
-	}
+    'hTrkPixelDedx_charginomatch_barrel',
+    'hTrkPixelDedxCalib_charginomatch_barrel',
+    'hTrkStripsDedx_charginomatch_barrel',
+	
+    'hTrkPixelDedx_charginomatch_endcap',
+    'hTrkPixelDedxCalib_charginomatch_endcap',
+    'hTrkStripsDedx_charginomatch_endcap',
 
+    # Muon
+    #'hMuP',
+    #'hMuPt',
+    #'hMuEta',
+    #'hMuPhi',
+    #
+    #'hMuP_genmatch',
+    #'hMuPt_genmatch',
+    #'hMuEta_genmatch',
+    #'hMuPhi_genmatch',
+    #
+    #'hTrkP_tightmumatch',
+    #'hTrkP_tightmumatch_barrel',
+    #'hTrkP_tightmumatch_endcap',
 
+    #'hTrkP_tightgenmumatch',
+    #'hTrkP_tightgenmumatch_barrel',
+    #'hTrkP_tightgenmumatch_endcap',
 
-def main(SelectedFastSim,SelectedFullSim,hist,outputdir):
+    #'hTrkPt_tightmumatch',
+    #'hTrkPt_tightmumatch_barrel',
+    #'hTrkPt_tightmumatch_endcap',
+    #
+    ##'hTrkEta_tightmumatch',
+    ##'hTrkEta_tightmumatch_barrel',
+    ##'hTrkEta_tightmumatch_endcap',
+
+    #'hTrkPt_tightgenmumatch',
+    #'hTrkPt_tightgenmumatch_barrel',
+    #'hTrkPt_tightgenmumatch_endcap',
+    #
+    ##'hTrkEta_tightgenmumatch',
+    ##'hTrkEta_tightgenmumatch_barrel',
+    ##'hTrkEta_tightgenmumatch_endcap',
+
+    #'hTrkPixelDedx_tightmumatch',
+    #'hTrkPixelDedx_tightmumatch_barrel',
+    #'hTrkPixelDedx_tightmumatch_endcap',
+    #'hTrkPixelDedxCalib_tightmumatch',
+    #'hTrkPixelDedxCalib_tightmumatch_barrel',
+    #'hTrkPixelDedxCalib_tightmumatch_endcap',
+
+    #'hTrkPixelDedx_tightgenmumatch',
+    #'hTrkPixelDedx_tightgenmumatch_barrel',
+    #'hTrkPixelDedx_tightgenmumatch_endcap',
+    #'hTrkPixelDedxCalib_tightgenmumatch',
+    #'hTrkPixelDedxCalib_tightgenmumatch_barrel',
+    #'hTrkPixelDedxCalib_tightgenmumatch_endcap',
+
+    #'hTrkStripsDedx_tightmumatch',
+    #'hTrkStripsDedx_tightmumatch_barrel',
+    #'hTrkStripsDedx_tightmumatch_endcap',
+
+    #'hTrkStripsDedx_tightgenmumatch',
+    #'hTrkStripsDedx_tightgenmumatch_barrel',
+    #'hTrkStripsDedx_tightgenmumatch_endcap',
+    ]
+
+# Run
+for hist in hists:
     
     print 'Drawing hist : ',hist
 
-    ctitle = 'FullSim/FastSim comparison'
-    c = TCanvas(ctitle,ctitle,800,600)
-    tl = TLegend(0.6,0.7,0.85,0.9)
+    canvas = rt.TCanvas("c2","c2",50,50,W,H)
+    canvas.SetFillColor(0)
+    canvas.SetBorderMode(0)
+    canvas.SetFrameFillStyle(0)
+    canvas.SetFrameBorderMode(0)
+    canvas.SetLeftMargin( L/W )
+    canvas.SetRightMargin( R/W )
+    canvas.SetTopMargin( T/H )
+    canvas.SetBottomMargin( B/H )
+    canvas.SetTickx(0)
+    canvas.SetTicky(0)
+    
+    tl = rt.TLegend(0.6,0.7,0.85,0.9)
 
     #c.SetLogy()
-    
-    fin={}
-    histos={}
-     
-    # Fastsim MC
-    i=0
-    for name,f in sorted(SelectedFastSim.items()):
-        fin[name] = TFile(f)
-        histos[name] = fin[name].Get(hist)
-	if i==0 :
-	    print 'Cloning ',name
-	    hFastsim = histos[name].Clone('hFastsim')
-	else : 
-	    print 'Adding ',name
-	    hFastsim.Add(histos[name])
-	i+=1
-    
-    hFastsim.GetXaxis().SetTitle('GeV')
-    hFastsim.GetYaxis().SetTitle('Normalized')
-    hFastsim.Scale(1.0/hFastsim.Integral())
+    h1 = f1.Get(hist) 
+    h1.GetXaxis().SetTitle('GeV')
+    h1.GetYaxis().SetTitle('Normalized')
+    h1.Scale(1.0/h1.Integral())
 
+    h2 = f2.Get(hist) 
+    h2.GetXaxis().SetTitle('GeV')
+    h2.GetYaxis().SetTitle('Normalized')
+    h2.Scale(1.0/h2.Integral())
 
-    # Fullsim MC
-    i = 0
-    for name,f in sorted(SelectedFullSim.items()):
-        fin[name] = TFile(f)
-	histos[name] = fin[name].Get(hist) 
-	if i==0:
-	    print 'Cloning ',name
-	    hFullsim = histos[name].Clone('hFullsim')
-	else : 
-	    print 'Adding ',name
-	    hFullsim.Add(histos[name])
-	i+=1
-
-    hFullsim.Rebin(rebin)
-    hFastsim.Rebin(rebin)
+    h1.Rebin(rebin)
+    h2.Rebin(rebin)
     
-    hFullsim.GetXaxis().SetTitle('GeV')
-    #hFullsim.GetXaxis().SetRangeUser(0,15)
-    hFullsim.GetYaxis().SetTitle('Normalized')
-    hFullsim.Scale(1.0/hFullsim.Integral())
+    h1.GetXaxis().SetTitle('GeV')
+    #h1.GetXaxis().SetRangeUser(0,15)
+    h1.GetYaxis().SetTitle('Normalized')
+    h1.Scale(1.0/h1.Integral())
 
-    hFastsim.SetLineColor(kRed)
-    hFullsim.SetLineColor(kBlue)
-    #hFullsim.SetFillStyle(3002)
-    #hFullsim.SetFillColor(kBlue)
+    h2.SetLineColor(rt.kRed)
+    h1.SetLineColor(rt.kBlue)
+    #h1.SetFillStyle(3002)
+    #h1.SetFillColor(kBlue)
     
-    rp = TRatioPlot(hFullsim,hFastsim)
+    rp = rt.TRatioPlot(h1,h2)
     rp.SetH1DrawOpt('E')
     rp.SetH2DrawOpt('E')
     rp.Draw()
@@ -110,102 +203,10 @@ def main(SelectedFastSim,SelectedFullSim,hist,outputdir):
     rp.GetLowerRefGraph().SetMinimum(0);
     rp.GetLowerRefGraph().SetMaximum(2);
    
-    tl.AddEntry(hFullsim,'T2bt Fullsim','lE')
-    tl.AddEntry(hFastsim,'T2bt FastSim','lE')
-    tl.Draw()
+    #tl.AddEntry(h1,'T2bt Fullsim','lE')
+    #tl.AddEntry(h2,'T2bt FastSim','lE')
+    #tl.Draw()
 
-    c.Update()
-    c.Print(outputdir+'/RatioPlot_'+hist+'.'+format_c)
-    c.Close()
-
-    
-if __name__ == '__main__' :
-
-    if not os.path.exists(outputdir) : os.system('mkdir -p '+outputdir)
-
-    SelectedFullSim = dict_Summer16_FullSimSignal
-    SelectedFastSim = dict_Summer16_FastSimSignal
-    
-    hists=[
-        #'hGenStopMass',
-        #'hGenLSPMass',
-        #'hGenCharginoP',
-        #'hGenCharginoPt',
-        #'hGenCharginoEta',
-        #'hGenCharginoPhi',
-    	#
-        #'hTrkP_charginomatch',
-        #'hTrkPt_charginomatch',
-        #'hTrkEta_charginomatch',
-        #'hTrkPhi_charginomatch',
-
-        'hTrkPixelDedx_charginomatch_barrel',
-    	'hTrkPixelDedxCalib_charginomatch_barrel',
-    	'hTrkStripsDedx_charginomatch_barrel',
-    	
-	'hTrkPixelDedx_charginomatch_endcap',
-    	'hTrkPixelDedxCalib_charginomatch_endcap',
-    	'hTrkStripsDedx_charginomatch_endcap',
-
-	# Muon
-	#'hMuP',
-	#'hMuPt',
-	#'hMuEta',
-	#'hMuPhi',
-	#
-	#'hMuP_genmatch',
-	#'hMuPt_genmatch',
-	#'hMuEta_genmatch',
-	#'hMuPhi_genmatch',
-	#
-	#'hTrkP_tightmumatch',
-	#'hTrkP_tightmumatch_barrel',
-	#'hTrkP_tightmumatch_endcap',
-
-	#'hTrkP_tightgenmumatch',
-	#'hTrkP_tightgenmumatch_barrel',
-	#'hTrkP_tightgenmumatch_endcap',
-
-	#'hTrkPt_tightmumatch',
-	#'hTrkPt_tightmumatch_barrel',
-	#'hTrkPt_tightmumatch_endcap',
-	#
-	##'hTrkEta_tightmumatch',
-	##'hTrkEta_tightmumatch_barrel',
-	##'hTrkEta_tightmumatch_endcap',
-
-	#'hTrkPt_tightgenmumatch',
-	#'hTrkPt_tightgenmumatch_barrel',
-	#'hTrkPt_tightgenmumatch_endcap',
-	#
-	##'hTrkEta_tightgenmumatch',
-	##'hTrkEta_tightgenmumatch_barrel',
-	##'hTrkEta_tightgenmumatch_endcap',
-
-	'hTrkPixelDedx_tightmumatch',
-	'hTrkPixelDedx_tightmumatch_barrel',
-	'hTrkPixelDedx_tightmumatch_endcap',
-	'hTrkPixelDedxCalib_tightmumatch',
-	'hTrkPixelDedxCalib_tightmumatch_barrel',
-	'hTrkPixelDedxCalib_tightmumatch_endcap',
-
-	'hTrkPixelDedx_tightgenmumatch',
-	'hTrkPixelDedx_tightgenmumatch_barrel',
-	'hTrkPixelDedx_tightgenmumatch_endcap',
-	'hTrkPixelDedxCalib_tightgenmumatch',
-	'hTrkPixelDedxCalib_tightgenmumatch_barrel',
-	'hTrkPixelDedxCalib_tightgenmumatch_endcap',
-
-	'hTrkStripsDedx_tightmumatch',
-	'hTrkStripsDedx_tightmumatch_barrel',
-	'hTrkStripsDedx_tightmumatch_endcap',
-
-	'hTrkStripsDedx_tightgenmumatch',
-	'hTrkStripsDedx_tightgenmumatch_barrel',
-	'hTrkStripsDedx_tightgenmumatch_endcap',
-        ]
-    
-    # Run
-    for hist in hists:
-        main(SelectedFastSim,SelectedFullSim,hist,outputdir)
-
+    canvas.Update()
+    canvas.Print(outputdir+'/RatioPlot_'+hist+'.'+format_c)
+    canvas.Close()
