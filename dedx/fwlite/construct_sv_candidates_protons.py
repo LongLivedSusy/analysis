7e40eb0918bfb095148f951175908f8a2ce21567
Sang Il Pak <spak@cern.ch>
2020-11-27 03:11:25 +0100

    update

--- dedx/fwlite/construct_sv_candidates_protons.py
+++ dedx/fwlite/construct_sv_candidates_protons.py
@@ -36,19 +36,11 @@ else:
 
 # Use the tracks_and_vertices.root file as input.
 process.source = cms.Source("PoolSource",
-#fileNames = cms.untracked.vstring()
-#fileNames = cms.untracked.vstring("file:/nfs/dust/cms/user/beinsam/CommonSamples/MC_BSM/CompressedHiggsino/RadiativeMu_2016Fast/v2/higgsino94x_susyall_mChipm500GeV_dm5p33GeV_pu35_part12of25.root")
 #fileNames = cms.untracked.vstring("root://cmsxrootd.fnal.gov//store/data/Run2016G/SingleElectron/AOD/07Aug17-v1/110000/D25CC9A3-5787-E711-AD38-20CF3027A589.root")
 fileNames =  cms.untracked.vstring(fname)
     )
 
 #more data file names for the example SingleEl 2016G are in the fileinfo directory
-
-#SUSY FullSim signals
-#root://cmsxrootd.fnal.gov//store/mc/RunIISummer16DR80Premix/SMS-T2bt-LLChipm_ctau-50_mLSP-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/AODSIM/PUMoriond17_longlived_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v2/40000/F219C63C-8A8A-E911-A7CA-008CFAF292B2.root
-#/store/mc/RunIISummer16DR80Premix/SMS-T2bt-LLChipm_ctau-50_mLSP-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/AODSIM/PUMoriond17_longlived_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v2/40000/ECF10E5A-6B8A-E911-BB0D-3417EBE528B5.root
-#/store/mc/RunIISummer16DR80Premix/SMS-T2bt-LLChipm_ctau-50_mLSP-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/AODSIM/PUMoriond17_longlived_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v2/40000/B6BB1B78-9D8A-E911-B1E0-2047478D3908.root
-
 
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 #process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100))
