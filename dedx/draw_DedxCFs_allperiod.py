2493aba878c8b9336fa4b70c22efca252525b075
Sang Il Pak <spak@cern.ch>
2021-01-08 13:46:48 +0100

    update

--- dedx/draw_DedxCFs_allperiod.py
+++ dedx/draw_DedxCFs_allperiod.py
@@ -8,6 +8,7 @@ legend = mklegend_(x1=0.55, y1=0.7, x2=0.9, y2=0.9)
 hMuonDedxCFs_barrel = TH1F('hMuonDedxCFs_barrel','',16,0,16)
 hMuonDedxCFs_endcap = TH1F('hMuonDedxCFs_endcap','',16,0,16)
 hProtonDedxCFs_barrel = TH1F('hProtonDedxCFs_barrel','',16,0,16)
+hProtonDedxCFs_endcap = TH1F('hProtonDedxCFs_endcap','',16,0,16)
 for index, key in enumerate(sorted(DedxCorr_Pixel_barrel)):
     if not 'Run' in key: continue
     hMuonDedxCFs_barrel.SetBinContent(index,DedxCorr_Pixel_barrel[key])
@@ -18,8 +19,9 @@ for index, key in enumerate(sorted(DedxCorr_Pixel_endcap)):
     hMuonDedxCFs_endcap.SetBinContent(index,DedxCorr_Pixel_endcap[key])
     hMuonDedxCFs_endcap.GetXaxis().SetBinLabel(index,key)
 
-hProtonDedxCFs_barrel.SetBinContent(6,1.427)
-hProtonDedxCFs_barrel.SetBinContent(12,1.342)
+hProtonDedxCFs_barrel.SetBinContent(6,1.457) #Run2016G
+hProtonDedxCFs_endcap.SetBinContent(6,1.438) #Run2016G
+hProtonDedxCFs_barrel.SetBinContent(12,1.342) # Run2017F
 
 
 legend.AddEntry(hMuonDedxCFs_barrel,'muon barrel CF')
@@ -29,6 +31,7 @@ legend.AddEntry(hProtonDedxCFs_barrel,'proton(P>6) CF')
 histoStyler(hMuonDedxCFs_barrel,kBlue)
 histoStyler(hMuonDedxCFs_endcap,kGreen)
 histoStyler(hProtonDedxCFs_barrel,kRed)
+histoStyler(hProtonDedxCFs_endcap,kRed+2)
 stamp()
 
 hMuonDedxCFs_barrel.SetTitle('pixel dE/dx correction factors')
@@ -39,6 +42,7 @@ hMuonDedxCFs_barrel.GetYaxis().SetRangeUser(0.9,2.0)
 hMuonDedxCFs_barrel.Draw()
 hMuonDedxCFs_endcap.Draw('hsame')
 hProtonDedxCFs_barrel.Draw('hsame')
+hProtonDedxCFs_endcap.Draw('hsame')
 legend.Draw()
 #canvas.SaveAs("dEdxCF.png")
 canvas.SaveAs("dEdxCF.pdf")
