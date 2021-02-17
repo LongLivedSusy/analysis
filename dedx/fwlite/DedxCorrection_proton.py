fe4fd3aecaf437bf8afc3e24b58287e28d5d30fd
Sang Il Pak <spak@cern.ch>
2021-02-17 07:15:57 +0100

    update

--- dedx/fwlite/DedxCorrection_proton.py
+++ dedx/fwlite/DedxCorrection_proton.py
@@ -57,45 +57,40 @@ if __name__ == '__main__' :
     #format_c = 'pdf'
     format_c = 'png'
     
+    f_data_2016B = "./SV_rootfiles/vertex_Run2016B_SingleElectron.root" 
     f_data_2016G = "./SV_rootfiles/vertex_Run2016G_SingleElectron.root" 
     f_data_2017F = "./SV_rootfiles/vertex_Run2017F_SingleElectron.root" 
+    f_data_2018C = "./SV_rootfiles/vertex_Run2018C_EGamma.root" 
     f_sim = "./SV_rootfiles/vertex_Summer16_T2bt.root" 
 
     rebin=2
 
     hists=[
 	# ['HISTNAME','X-axis title',logy,rebin],
-	#['dedxpixel_good_proton_P0to0p5','Proton(P 0-0.5) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P0p5to1','Proton(P 0.5~1) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P1to1p5','Proton(P 1~1.5) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P1p5to2','Proton(P 1.5~2) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P2to3','Proton(P 2~3) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P3to4','Proton(P 3~4) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P4to5','Proton(P 4-5) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P5to6','Proton(P 5-6) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P6toInf','Proton(P 6-Inf) pixel dEdx [MeV]',False,rebin],
-	#['dedxpixel_good_proton_P0to0p5_barrel','Proton(P 0-0.5) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P0p5to1_barrel','Proton(P 0.5~1) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P1to1p5_barrel','Proton(P 1~1.5) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P1p5to2_barrel','Proton(P 1.5~2) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P2to3_barrel','Proton(P 2~3) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P3to4_barrel','Proton(P 3~4) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P4to5_barrel','Proton(P 4-5) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P5to6_barrel','Proton(P 5-6) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P6toInf_barrel','Proton(P 6-Inf) pixel dEdx [MeV]',False,rebin],
-	#['dedxpixel_good_proton_P0to0p5_endcap','Proton(P 0-0.5) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P0p5to1_endcap','Proton(P 0.5~1) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P1to1p5_endcap','Proton(P 1~1.5) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P1p5to2_endcap','Proton(P 1.5~2) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P2to3_endcap','Proton(P 2~3) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P3to4_endcap','Proton(P 3~4) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P4to5_endcap','Proton(P 4-5) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P5to6_endcap','Proton(P 5-6) pixel dEdx [MeV]',False,rebin],
-	['dedxpixel_good_proton_P6toInf_endcap','Proton(P 6-Inf) pixel dEdx [MeV]',False,rebin],
+	#['dedxpixel_good_proton_P1to2','Proton(P 1~2) pixel dEdx [MeV]',False,rebin],
+	#['dedxpixel_good_proton_P2to3','Proton(P 2~3) pixel dEdx [MeV]',False,rebin],
+	#['dedxpixel_good_proton_P3to4','Proton(P 3~4) pixel dEdx [MeV]',False,rebin],
+	#['dedxpixel_good_proton_P4to5','Proton(P 4~5) pixel dEdx [MeV]',False,rebin],
+	#['dedxpixel_good_proton_P5to6','Proton(P 5~6) pixel dEdx [MeV]',False,rebin],
+	#['dedxpixel_good_proton_P6toInf','Proton(P 6~Inf) pixel dEdx [MeV]',False,rebin],
+	#['dedxpixel_good_proton_P1to2_barrel','Proton(P 1~2) pixel dEdx [MeV]',False,rebin],
+	#['dedxpixel_good_proton_P2to3_barrel','Proton(P 2~3) pixel dEdx [MeV]',False,rebin],
+	#['dedxpixel_good_proton_P3to4_barrel','Proton(P 3~4) pixel dEdx [MeV]',False,rebin],
+	#['dedxpixel_good_proton_P4to5_barrel','Proton(P 4~5) pixel dEdx [MeV]',False,rebin],
+	#['dedxpixel_good_proton_P5to6_barrel','Proton(P 5~6) pixel dEdx [MeV]',False,rebin],
+	#['dedxpixel_good_proton_P6toInf_barrel','Proton(P 6~Inf) pixel dEdx [MeV]',False,rebin],
+	['dedxpixel_good_proton_P1to2_endcap','Proton(P 1~2) pixel dEdx [MeV]',False,rebin],
+	#['dedxpixel_good_proton_P2to3_endcap','Proton(P 2~3) pixel dEdx [MeV]',False,rebin],
+	#['dedxpixel_good_proton_P3to4_endcap','Proton(P 3~4) pixel dEdx [MeV]',False,rebin],
+	#['dedxpixel_good_proton_P4to5_endcap','Proton(P 4~5) pixel dEdx [MeV]',False,rebin],
+	#['dedxpixel_good_proton_P5to6_endcap','Proton(P 5~6) pixel dEdx [MeV]',False,rebin],
+	#['dedxpixel_good_proton_P6toInf_endcap','Proton(P 6~Inf) pixel dEdx [MeV]',False,rebin],
     ]
     
     # Run
     for hist in hists:
-        #main(f_data_2016G, hist, outputdir='./plots_DedxCorrection_proton_data_2016G')
-        main(f_data_2017F, hist, outputdir='./plots_DedxCorrection_proton_data_2017F')
-        #main(f_sim, hist, outputdir='./plots_DedxCorrection_proton_sim')
+        main(f_data_2016B, hist, outputdir='./plots_DedxCorrection_proton_2016B')
+        main(f_data_2016G, hist, outputdir='./plots_DedxCorrection_proton_2016G')
+        main(f_data_2017F, hist, outputdir='./plots_DedxCorrection_proton_2017F')
+        main(f_data_2018C, hist, outputdir='./plots_DedxCorrection_proton_2018C')
+        main(f_sim, hist, outputdir='./plots_DedxCorrection_proton_T2bt')
