f1d5ec8970cc410b383eac8c3078fb1fbf9a9211
Sam Bein <samuel.bein@gmail.com>
2021-04-12 07:15:17 +0200

    updated shared_utils.py for indentation glitch

--- tools/shared_utils.py
+++ tools/shared_utils.py
@@ -1449,16 +1449,16 @@ def Load_DedxSmear(phase):
 def Load_DedxSmear_MIH(phase):
 	print 'Loading dEdx smear function for phase:',phase
 	if phase==0 :
-	sigma_data_barrel = 0.533
-	sigma_data_endcap = 0.5114
-	sigma_mc_barrel = 0.5084
-	sigma_mc_endcap = 0.4182
+		sigma_data_barrel = 0.533
+		sigma_data_endcap = 0.5114
+		sigma_mc_barrel = 0.5084
+		sigma_mc_endcap = 0.4182
 
 	elif phase==1 :
-	sigma_data_barrel = 0.4403
-	sigma_data_endcap = 0.4647
-	sigma_mc_barrel = 0.3824
-	sigma_mc_endcap = 0.3104
+		sigma_data_barrel = 0.4403
+		sigma_data_endcap = 0.4647
+		sigma_mc_barrel = 0.3824
+		sigma_mc_endcap = 0.3104
 	else : print 'put correct phase', quit()
 
 	sigma_smear_barrel = TMath.Sqrt(sigma_data_barrel**2-sigma_mc_barrel**2)
